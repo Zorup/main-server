99361f1ac05847b1898ece4287271418fb5c3fd6
lophunju <lophunju@naver.com>
2021-04-11 18:37:53 +0900

    안쓰는 테스트 삭제

--- src/test/java/com/example/socialpost/SocialpostApplicationTests.java
+++ src/test/java/com/example/socialpost/SocialpostApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SocialpostApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
