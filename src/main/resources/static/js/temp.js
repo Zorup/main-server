4994795e5094b9f40d1a8476d3e811a147593b62
Kim-hasa <62535495+Kim-hasa@users.noreply.github.com>
2021-05-18 18:00:07 +0900

    5/18
    scroll 외

--- src/main/resources/static/js/temp.js
+++ src/main/resources/static/js/temp.js
@@ -1,3 +1,4 @@
+
 let deleteForumList = [];
 
 $('input[name=fList]').click(function(){
@@ -106,36 +107,19 @@ function ChangeName(){
     });
 }
 
-$(document).ready(function(){
-
-      var list = $(".list li");
-      var numToShow = 3;
-      var button = $("#next");
-      var numInList = list.length;
-      list.hide();
-      if (numInList > numToShow) {
-        button.show();
-      }
-      list.slice(0, numToShow).show();
-
-      button.click(function(){
-          var showing = list.filter(':visible').length;
-          list.slice(showing - 1, showing + numToShow).fadeIn();
-          var nowShowing = list.filter(':visible').length;
-          if (nowShowing >= numInList) {
-            button.hide();
-          }
-      });
-
-});
-
+// show more
 $(".posts").slice(0,3).show();
 
-$(".showMore").on("click",function(){
-    $(".posts:hidden").slice(0, 5).show();
-
-    if($(".posts:hidden").length == 0)
+$(window).scroll(function(){
+    if($(window).scrollTop()+1 >= $(document).height() - $(window).height()) // 아래에서 200 떨어져 있을때 불러오고 싶음
     {
-        $(".showMore").fadeOut();
+        $(".posts:hidden").slice(0, 5).show();
+        console.log("scroll");
+            if($(".posts:hidden").length == 0)
+            {
+                $(".showMore").fadeOut();
+                console.log("fin");
+            }
     }
 })
+
