eaeeae3c222ea01751dd252f4e0f674ef95af001
lophunju <lophunju@naver.com>
2021-11-02 04:48:13 +0900

    [기능추가] 덧글 삭제 API

--- src/main/java/com/example/socialpost/service/CommentService.java
+++ src/main/java/com/example/socialpost/service/CommentService.java
@@ -43,4 +43,8 @@ public class CommentService {
 
         return new Comment.CommentResponse(commentJpaRepo.save(newItem));
     }
+
+    public void deleteComment(Long commentId){
+        commentJpaRepo.deleteById(commentId);
+    }
 }
